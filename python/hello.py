fb70c323fd51c213eaafa6597f3824dbac9e6081
Afariza HALIM <afarizahalim@gmail.com>
2022-12-11 18:46:03 +0100

    First python code of introductory commandes

---
+++ python/hello.py
@@ -0,0 +1,14 @@
+print('Hello world!')
+
+#Ask for their name
+print('What is your name?')
+name = input()
+print('It is good to meet you, ' +name)
+
+namelength=len(name)
+print('The length of your name is: ' + str(int(namelength)))
+
+#Ask for their age
+print('What is your age?')
+age = input()
+print('You will be ' + str(int(age) + 1)+ ' in a year')
